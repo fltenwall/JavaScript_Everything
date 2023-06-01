98328d3221e36069afbb06c9e9273070586d7559
fltenwall <fltenwall@163.com>
2023-06-01 22:38:57 +0800

    作用域链

--- notes/JavaScript/test.js
+++ notes/JavaScript/test.js
@@ -1,3 +1,16 @@
-let str= 'aaaddcc'
-let newStr = foo.replaceAll('a','p')
-console.log(newStr) // pppddcc
+console.log(a,b)
+var a=12,b=12;
+function fn(){
+    console.log(a,b)
+    var a = b = 13
+    console.log(a,b)
+}
+fn()
+console.log(a,b)
+
+/*
+undefined undefined
+undefined 12
+13 13
+12 13
+*/
