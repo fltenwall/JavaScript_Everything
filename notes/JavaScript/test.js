608a00dc90fa79a5da6f6a8e5e8134f0f4fb218c
fltenwall <fltenwall@163.com>
2023-06-06 02:20:06 +0800

    事件循环

--- notes/JavaScript/test.js
+++ notes/JavaScript/test.js
@@ -1,31 +1,13 @@
-async function async1(){
-    console.log('async1 start')
-    await async2()
-    console.log('async1 end')
-}
-async function async2(){
-    console.log('async2')
-}
-console.log('script start')
-setTimeout(function(){
-    console.log('setTimeout')
-},0)
-async1()
-new Promise(function(resolve){
-    console.log('promise1')
-    resolve()
-}).then(function(){
-    console.log('promise2')
+let body = document.body;
+body.addEventListener('click', function(){
+    Promise.resolve().then(()=>{
+        console.log(1)
+    })
+    console.log(2)
 })
-console.log('script end')
-
-/*
-console.log('promise1')
-console.log('async1 start')
-console.log('async2')
-console.log('promise1')
-console.log('script end')
-console.log('async1 end')
-console.log('promise2')
-console.log('setTimeout')
-*/
+body.addEventListener('click', function(){
+    Promise.resolve().then(()=>{
+        console.log(3)
+    })
+    console.log(4)
+})
